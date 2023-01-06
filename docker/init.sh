182a776e3cce6c5d31097a633e9a65e03dc646ed
SuperDev <supernova@supers0ft.us>
2023-01-06 22:58:57 +0000

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-echo -e "Initializing App"
+echo -e " ______                                       _ " 
+echo -e "|  ____|                                     | |"
+echo -e "| |__   __ _ ___ _   _ _ __   __ _ _ __   ___| |"
+echo -e "|  __| / _` / __| | | | '_ \ / _` | '_ \ / _ \ |"
+echo -e "| |___| (_| \__ \ |_| | |_) | (_| | | | |  __/ |"
+echo -e "|______\__,_|___/\__, | .__/ \__,_|_| |_|\___|_|"
+echo -e "                  __/ | |                       "
+echo -e "                 |___/|_|                       "
+ 
+sleep 5
 
-cd ../
-composer update
-npm ci
-php artisan route:cache
-php artisan view:clear
-php artisan migrate --force
+echo -e "[INFO] Initializing App"
+
+echo -e "[INFO ]Installing Packages"
+composer update --quiet
+npm ci -- silent
+
+echo -e "[INFO] Clearing Route Cache"
+php artisan route:cache -- quiet
+
+echo -e "[INFO] Clearing View Cache"
+php artisan view:clear -- quiet
+
+echo -e "[INFO] Migrating Database"
+php artisan migrate --force -- quiet
+
+echo -e "[INFO] Initialization Has Finished"
+
