31beaaef76bb920027958db78088667283b5104f
SuperDev <supernova@supers0ft.us>
2023-01-06 23:49:51 +0000

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-echo -e " ______                                       _ " 
-echo -e "|  ____|                                     | |"
-echo -e "| |__   __ _ ___ _   _ _ __   __ _ _ __   ___| |"
-echo -e "|  __| / _` / __| | | | '_ \ / _` | '_ \ / _ \ |"
-echo -e "| |___| (_| \__ \ |_| | |_) | (_| | | | |  __/ |"
-echo -e "|______\__,_|___/\__, | .__/ \__,_|_| |_|\___|_|"
-echo -e "                  __/ | |                       "
-echo -e "                 |___/|_|                       "
+echo -e "EEEEEEE                                                      lll " 
+echo -e "EE        aa aa  sss  yy   yy pp pp     aa aa nn nnn    eee  lll "
+echo -e "EEEEE    aa aaa s     yy   yy ppp  pp  aa aaa nnn  nn ee   e lll "
+echo -e "EE      aa  aaa  sss   yyyyyy pppppp  aa  aaa nn   nn eeeee  lll "
+echo -e "EEEEEEE  aaa aa     s      yy pp       aaa aa nn   nn  eeeee lll"
+echo -e "                 sss   yyyyy  pp                                 "
  
 sleep 5
 
 echo -e "[INFO] Initializing App"
 
-echo -e "[INFO ]Installing Packages"
+echo -e "[INFO] Installing Packages"
 composer update --quiet
 npm ci -- silent
 
